be736a7cb23e30ebc2ae220ed00c9efddf76801b
fatekkl <mathtml.1105@gmail.com>
2024-06-11 12:54:15 -0300

    build: config hibernate

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -3,19 +3,25 @@ package org.example;
 import org.example.models.Funcionario;
 import org.example.models.Pessoa;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
 
 public class Principal {
     public static void main(String[] args) {
         Pessoa pessoa;
         Funcionario funcionario;
+        Configuration configuration = new Configuration().configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
 
 
+        if (sessionFactory != null){
+            System.out.println("Conexão feita!");
+        }
 
-
-
-        System.out.println();
     }
 
 }
