ec8ce55e6156c5718a8ec26199a26df9b433ef41
fatekkl <mathtml.1105@gmail.com>
2024-06-11 23:11:47 -0300

    feat: update remove method & populate

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -27,24 +27,39 @@ public class Principal {
         BigDecimal salario_heloisa = BigDecimal.valueOf( 1606.85 );
         BigDecimal salario_helena = BigDecimal.valueOf( 2799.93 );
 
-        Funcionario maria = new Funcionario( "Maria", LocalDate.of( 2000, 10, 18 ), salario_maria, "Operador", 1 );
-        Funcionario joao = new Funcionario( "Joao", LocalDate.of( 1990, 5, 12 ), salario_joao, "Operador", 2 );
-        Funcionario caio = new Funcionario( "Caio", LocalDate.of( 1961, 5, 2 ), salario_caio, "Coordenador", 3 );
-        Funcionario miguel = new Funcionario( "Miguel", LocalDate.of( 1988, 10, 14 ), salario_miguel, "Diretor", 4 );
-        Funcionario alice = new Funcionario( "Alice", LocalDate.of( 1995, 1, 5 ), salario_alice, "Recepcionista", 5 );
-        Funcionario heitor = new Funcionario( "Heitor", LocalDate.of( 1999, 11, 19 ), salario_heitor, "Operador", 6 );
-        Funcionario arthur = new Funcionario( "Arthur", LocalDate.of( 1993, 3, 31 ), salario_arthur, "Contador", 7 );
-        Funcionario laura = new Funcionario( "Laura", LocalDate.of( 1994, 7, 19 ), salario_laura, "Gerente", 8 );
-        Funcionario heloisa = new Funcionario( "Heloisa", LocalDate.of( 2003, 5, 24 ), salario_heloisa, "Eletricista", 9 );
-        Funcionario helena = new Funcionario( "Helena", LocalDate.of( 1996, 9, 2 ), salario_helena, "Gerente", 10 );
+
+        Funcionario maria = new Funcionario( "Maria", LocalDate.of( 2000, 10, 18 ), salario_maria, "Operador" );
+        Funcionario joao = new Funcionario( "Joao", LocalDate.of( 1990, 5, 12 ), salario_joao, "Operador" );
+        Funcionario caio = new Funcionario( "Caio", LocalDate.of( 1961, 5, 2 ), salario_caio, "Coordenador" );
+        Funcionario miguel = new Funcionario( "Miguel", LocalDate.of( 1988, 10, 14 ), salario_miguel, "Diretor" );
+        Funcionario alice = new Funcionario( "Alice", LocalDate.of( 1995, 1, 5 ), salario_alice, "Recepcionista" );
+        Funcionario heitor = new Funcionario( "Heitor", LocalDate.of( 1999, 11, 19 ), salario_heitor, "Operador" );
+        Funcionario arthur = new Funcionario( "Arthur", LocalDate.of( 1993, 3, 31 ), salario_arthur, "Contador" );
+        Funcionario laura = new Funcionario( "Laura", LocalDate.of( 1994, 7, 19 ), salario_laura, "Gerente" );
+        Funcionario heloisa = new Funcionario( "Heloisa", LocalDate.of( 2003, 5, 24 ), salario_heloisa, "Eletricista" );
+        Funcionario helena = new Funcionario( "Helena", LocalDate.of( 1996, 9, 2 ), salario_helena, "Gerente" );
 
 
         SessionFactory sessionFactory = Banco.getSessionFactory();
 
         FuncionariosDAO funcionariosDAO = new FuncionariosDAO();
 
-        funcionariosDAO.adicionar( joao, sessionFactory );
+        // ADICIONA FUNCIONARIOS NA ORDEM
+//        funcionariosDAO.adicionar( joao, sessionFactory );
+//        funcionariosDAO.adicionar( caio, sessionFactory );
+//        funcionariosDAO.adicionar( miguel, sessionFactory );
+//        funcionariosDAO.adicionar( alice, sessionFactory );
+//        funcionariosDAO.adicionar( heitor, sessionFactory );
+//        funcionariosDAO.adicionar( arthur, sessionFactory );
+//        funcionariosDAO.adicionar( laura, sessionFactory );
+//        funcionariosDAO.adicionar( heloisa, sessionFactory );
+          funcionariosDAO.adicionar( helena, sessionFactory );
+//
+//        // REMOVE O JOAO
+//        funcionariosDAO.remover(2, sessionFactory );
 
+
+        sessionFactory.close();
 
     }
 
--- src/main/java/org/example/data/FuncionariosDAO.java
+++ src/main/java/org/example/data/FuncionariosDAO.java
@@ -9,7 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import java.util.List;
 
-public class FuncionariosDAO{
+public class FuncionariosDAO {
 
 
     public String getFuncionarios(SessionFactory sessionFactory) {
@@ -19,7 +19,7 @@ public class FuncionariosDAO{
         try {
             Session session = sessionFactory.openSession();
 
-            Query query = session.createQuery("FROM Funcionario", Funcionario.class);
+            Query query = session.createQuery( "FROM Funcionario", Funcionario.class );
 
             funcionarios = query.getResultList();
 
@@ -35,28 +35,51 @@ public class FuncionariosDAO{
             Session session = sessionFactory.openSession();
             Transaction transaction = session.beginTransaction();
 
-            session.save(funcionario);
+            session.save( funcionario );
 
             transaction.commit();
             session.close();
 
-            System.out.println("Funcionário adicionado a tabela!");
-        } finally {
-            sessionFactory.close();
+            System.out.println( "Funcionário adicionado a tabela!" );
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public void remover(Funcionario funcionario, SessionFactory sessionFactory) {
-        try {
-            Session session = sessionFactory.openSession();
+    public void remover(int id, SessionFactory sessionFactory) {
+
+        try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
-            session.delete(funcionario);
-            transaction.commit();
-            session.close();
 
-            System.out.println("Funcionário removido da tabela!");
-        } finally {
-            sessionFactory.close();
+            Query query = session.createQuery( "delete from Funcionario where id = :id" );
+            query.setParameter( "id", id );
+            int result = query.executeUpdate();
+
+            transaction.commit();
+            if (result > 0) {
+                System.out.println( "Funcionário removido" );
+            } else {
+                System.out.println( "Nenhum funcionário encontrado com o ID fornecido" );
+            }
         }
     }
+
+    public void removerPorNome(String nome, SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            Query query = session.createQuery( "delete from Funcionario where nome = :nome" );
+            query.setParameter( "nome", nome );
+            int result = query.executeUpdate();
+
+            transaction.commit();
+            if (result > 0) {
+                System.out.println( "Funcionário removido" );
+            } else {
+                System.out.println( "Nenhum funcionário encontrado com o ID fornecido" );
+            }
+        }
+    }
+
+
 }
